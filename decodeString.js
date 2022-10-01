96820fd959f9d6ccd5fb65e7ab1d16ce385f57a9
Christina Woodhams <cwoodhams89@gmail.com>
2022-09-30 20:42:48 -0400

    Third commit. Fixed typos in explanation.

--- decodeString.js
+++ decodeString.js
@@ -15,9 +15,9 @@ console.log(decodeString("2fcjjm"))
 
 // This function decodes a string by using index[0] to determine how many ascii units each letter should increase by.
 // Since index[0] is a string, and not a number, it must first be set to a number. 
-// To set index[0] to a string you can use +, NUM, or parseINT(string[0]).
+// To set index[0] to a number you can use +, NUM, or parseINT(string[0]).
 // Create an empty variable, that accepts a string, for your answer. 
 // Now, loop through your array starting at index  1, in order to avoid the number. 
 // Create a variable to store the ascii value for each index, plus the number. 
-// Push the array back into a string using the formCharCode method. 
+// Push the array back into a string using the String and formCharCode methods. 
 // Return the result. 
